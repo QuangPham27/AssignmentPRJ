12161fee107baad12b590891afda2e277d09ead1
Quang <phamquang1375@gmail.com>
2022-10-26 21:21:00 +0700

    hello

--- src/java/model/Account.java
+++ src/java/model/Account.java
@@ -18,7 +18,7 @@ public class Account {
     }
 
     public void setUsername(String username) {
-        this.username = username;
+        this.username = username.toLowerCase();
     }
 
     public String getPassword() {
